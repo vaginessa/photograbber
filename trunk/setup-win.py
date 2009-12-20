5133c28c9f1cfda0b4b8da759e91ae51d2a03564
tommy.murphy <tommy.murphy@9c6953bc-b531-11de-8be4-e1425cda3908>
2009-12-20 01:27:19 +0000

    fixed windows distribution script and added missing creepon.ppm

--- trunk/setup-win.py
+++ trunk/setup-win.py
@@ -4,7 +4,7 @@ import py2exe, sys, os
 sys.argv.append('py2exe')
 
 setup(
-        data_files = [("img",["img/1.ppm","img/2.ppm","img/3.ppm","img/4.ppm","img/pg.ico"])],
+        data_files = [("img",["img/creepon.ppm","img/download.ppm","img/folder.ppm","img/login.ppm","img/quit.ppm","img/pg.ico"])],
         options = {'py2exe': {'optimize':2}},
         windows = [{'script': 'pg.py'}],
         zipfile = None,
