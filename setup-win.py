76bb1e05904e28cb77e482a09b133cc700d7cae1
tommy.murphy <tommy.murphy@9c6953bc-b531-11de-8be4-e1425cda3908>
2010-01-06 22:47:57 +0000

    Fixed some organizational things

--- setup-win.py
+++ setup-win.py
@@ -4,7 +4,7 @@ import py2exe, sys, os
 sys.argv.append('py2exe')
 
 setup(
-        data_files = [("img",["img/creepon.ppm","img/download.ppm","img/login.ppm","img/quit.ppm","img/pg.ico"],"EXTRA/INSTALL.txt","EXTRA/LICENSE.txt")],
+        data_files = [("img",["img/creepon.ppm","img/download.ppm","img/login.ppm","img/quit.ppm","img/pg.ico"])],
         options = {'py2exe': {'optimize':2}},
         windows = [{'script': 'pg.py'}],
         zipfile = None,
