b73642f2578d76b8b3132f084a2e813d77d080f4
tommy.murphy <tommy.murphy@9c6953bc-b531-11de-8be4-e1425cda3908>
2010-06-16 18:03:50 +0000

    added JSON dump and concurrent downloading (within an album). now uses Python's logging module and some fixes for thread termination

--- trunk/downloader.py
+++ trunk/downloader.py
@@ -1,15 +1,15 @@
 import os, re, sys, urllib2, time
 from threading import Thread
+import multiprocessing
+import albumhelpers
+import logging
 
 class FBDownloader(Thread):
     REPLACE_RE = re.compile(r'\*|"|\'|:|<|>|\?|\\|/|\|,| ')
-    CAPTION = -1
-    DESCRIPTION = -2
-    LOCATION = -3
 
     def __init__ (self, photos_path, uid, friends,
                         full_albums, user_albums, extras, facebook,
-                        update_callback, error_callback, force_exit_callback):
+                        update_callback, force_exit_callback):
         Thread.__init__(self)
         self.photos_path = photos_path
         self.uid = uid
@@ -21,40 +21,37 @@ class FBDownloader(Thread):
         self.extras = extras
         # callback functions
         self.update = update_callback
-        self.error = error_callback
         self.force_exit = force_exit_callback
 
         self._thread_terminated = False
         self.index = self.total = 0
         self.albums = {}        # hold all pic data
 
-    def retry_function(self, max_retries, function, *args, **kw):
+    # terminate the thread if need be
+    def exit_if_terminated(self):
+        if self._thread_terminated:
+            logging.info('GUI terminating download thread')
+            if self.po:
+               self.po.terminate() # stop workers
+            sys.exit() # raise SystemExit exception to terminate run()
+
+    # queries fail a lot, retry them many times
+    def query_wrapper(self, q):
+        max_retries = 10
         retries = 0
         while True:
+            # check if thread should be terminated
             self.exit_if_terminated()
             try:
-                return function(*args, **kw)
+                return self.facebook.fql.query(q)
             except Exception, e:
                 if retries < max_retries:
-                    self.error('retrying function: %d\n' % retries, False)
+                    logging.info('retrying function: %d\n' % retries)
                     retries += 1
                     # sleep longer and longer between retries
                     time.sleep(retries * 2)
                 else:
                     raise
-
-    # terminate the thread if need be
-    def exit_if_terminated(self):
-        if self._thread_terminated:
-            sys.exit() # raise SystemExit exception to terminate run()
-
-    # queries fail a lot, lets retry them too
-    def query_wrapper(self, q):
-        try:
-            return self.retry_function(10, self.facebook.fql.query, q)
-        except Exception, e:
-            # maybe do something different with error...
-            self.error(e)
 
     # return a persons name
     def friend_name(self, uid):
@@ -62,12 +59,7 @@ class FBDownloader(Thread):
             # this should never happen... but it did once
             return 'Unknown'
         if uid not in self.friends:
-            q = 'SELECT name FROM profile WHERE id=%s'
-            res = self.query_wrapper(q % uid)
-            if res:
-                self.friends[uid] = res[0]['name']
-            else:
-                self.friends[uid] = 'uid_%s' % uid # could not find real name
+            self.friends[uid] = albumhelpers.get_friend(self.query_wrapper, uid)
         return self.friends[uid]
 
     # functions to write extra info
@@ -75,11 +67,11 @@ class FBDownloader(Thread):
     def write_comments(self, filename, comments):
         fp = open(filename, 'w')
         for comment in sorted(comments, key=lambda x:x['time']):
-            if comment['fromid'] == self.CAPTION:
+            if comment['fromid'] == albumhelpers.CAPTION:
                 fp.write('Photo Caption\n')
-            elif comment['fromid'] == self.DESCRIPTION:
+            elif comment['fromid'] == albumhelpers.DESCRIPTION:
                 fp.write('Album Description\n')
-            elif comment['fromid'] == self.LOCATION:
+            elif comment['fromid'] == albumhelpers.LOCATION:
                 fp.write('Album Location\n')
             else:
                 friend = self.friend_name(comment['fromid'])
@@ -100,100 +92,27 @@ class FBDownloader(Thread):
 
 
     # get albums and photos
-    # heavy FQL statements
 
     def get_albums(self):
-        # all albums the user is tagged in
-        q = ''.join(['SELECT aid, owner, name, modified, description, ',
-                     'location, object_id FROM album WHERE aid IN (SELECT ',
-                     'aid FROM photo WHERE pid IN (SELECT pid FROM photo_tag ',
-                     'WHERE subject="%s"))']) % self.uid
-
-        for item in self.query_wrapper(q):
-            item['photos'] = {}
-            self.albums[item['aid']] = item
-
-
-        # all albums uploaded by the user
-        q = ''.join(['SELECT aid, owner, name, modified, description, ',
-                     'location, object_id FROM album WHERE ',
-                     'owner="%s"']) % self.uid
+        albumhelpers.get_tagged_albums(self.query_wrapper, self.uid, self.albums)
         if self.user_albums:
-            for item in self.query_wrapper(q):
-                item['photos'] = {}
-                self.albums[item['aid']] = item
+            albumhelpers.get_uploaded_albums(self.query_wrapper, self.uid, self.albums)
 
     def get_pictures(self):
-        # all pictures where the user is tagged
-        q = ''.join(['SELECT pid, aid, src_big, caption, created, object_id ',
-                     'FROM photo WHERE pid IN (SELECT pid FROM photo_tag ',
-                     'WHERE subject="%s")']) % self.uid
-
         if self.full_albums:
-            # full albums where the user is tagged
-            q = ''.join(['SELECT pid, aid, src_big, caption, created, ',
-                         'object_id FROM photo WHERE aid IN (SELECT aid ',
-                         'FROM photo WHERE pid IN (SELECT pid FROM photo_tag ',
-                         'WHERE subject="%s"))']) % self.uid
-
-        for photo in self.query_wrapper(q):
-            self.albums[photo['aid']]['photos'][photo['pid']] = photo
+            albumhelpers.get_tagged_album_pictures(self.query_wrapper, self.uid, self.albums)
+        else:
+            albumhelpers.get_tagged_pictures(self.query_wrapper, self.uid, self.albums)
 
         if self.user_albums:
-            # all pictures in albums uploaded by the user
-            q = ''.join(['SELECT pid, aid, src_big, caption, created, ',
-                         'object_id FROM photo WHERE aid IN (SELECT aid FROM ',
-                         'album WHERE owner="%s")']) % self.uid
-            for photo in self.query_wrapper(q):
-                self.albums[photo['aid']]['photos'][photo['pid']] = photo
+            albumhelpers.get_user_album_pictures(self.query_wrapper, self.uid, self.albums)
 
-    # yay
+    # process an album
     def save_album(self, album):
         self.exit_if_terminated()
 
-        # get album and photo comments
-        q = ''.join(['SELECT object_id, fromid, time, text FROM comment ',
-                     'WHERE object_id in (%s)'])
-
-        o2pid = {} # translate object_id to pid
-        oids = [] # hold object_ids to lookup
-
-        album_comments = []
-
-        for photo in album['photos'].values():
-            o2pid[photo['object_id']] = photo['pid']
-            oids.append('"%s"' % photo['object_id'])
-            if photo['caption']:
-                photo['comments'] = [{'fromid':self.CAPTION,
-                                      'text':photo['caption'], 'time':0}]
-
-        oids.append('"%s"' % album['object_id'])
-        if album['description']:
-            album_comments.append({'fromid':self.DESCRIPTION, 'time':0,
-                                   'text':album['description']})
-        if album['location']:
-            album_comments.append({'fromid':self.LOCATION, 'time':1,
-                                   'text':album['location']})
-
-        # do i want extra info?
         if self.extras:
-            # load all comments for album and its photos
-            for item in self.query_wrapper(q % ','.join(oids)):
-                oid = item['object_id']
-                if oid in o2pid: # photo comment
-                    clist = album['photos'][o2pid[oid]].setdefault('comments',
-                                                                   [])
-                    clist.append(item)
-                else: # album comment
-                    album_comments.append(item)
-
-            # load tags in each photo
-            q = ''.join(['SELECT pid, text, xcoord, ycoord FROM ',
-                         'photo_tag WHERE pid IN(%s)'])
-            pids = ['"%s"' % x for x in album['photos'].keys()]
-            for item in self.query_wrapper(q % ','.join(pids)):
-                tag_list = album['photos'][item['pid']].setdefault('tags', [])
-                tag_list.append(item)
+            albumhelpers.get_album_comments(self.query_wrapper, album)
 
         username = self.friend_name(album['owner'])
         album_folder = self.REPLACE_RE.sub(
@@ -204,26 +123,36 @@ class FBDownloader(Thread):
         if not os.path.isdir(album_path):
             os.mkdir(album_path)
 
-        # Save album comments
-        if album_comments:
+        # Save album comments, will only exist if we fetched them
+        if album['comments']:
             meta_path = os.path.join(album_path, 'ALBUM_COMMENTS.txt')
-            self.write_comments(meta_path, album_comments)
+            self.write_comments(meta_path, album['comments'])
 
+        # Concurrent download functionality
+        self.po = multiprocessing.Pool(processes=5)
+        
         for photo in album['photos'].items():
-            # update progress bar
-            self.update(self.index, self.total)
-            self.index += 1
-            # save photo
+            # photo is a tuple, [0] pid, [1] dictionary
             self.save_photo(album_path, *photo)
 
+        # Stop accepting more work
+        self.po.close()
+
+        # Wait for all workers to finish
+        while multiprocessing.active_children():
+            self.exit_if_terminated()
+            time.sleep(1)            
+        self.po.join()
+
         # Reset modify time after adding files
+        # (must happen after threads finish)
         os.utime(album_path, (int(album['modified']),) * 2)
 
-
+    # process a photo
     def save_photo(self, album_path, pid, photo):
         self.exit_if_terminated()
 
-        filename = os.path.join(album_path, '%s.jpg' % pid)
+        # Save extra info
         if 'comments' in photo:
             meta_name = os.path.join(album_path, '%s_comments.txt' % pid)
             self.write_comments(meta_name, photo['comments'])
@@ -232,20 +161,24 @@ class FBDownloader(Thread):
             meta_name = os.path.join(album_path, '%s_tags.txt' % pid)
             self.write_tags(meta_name, photo['tags'], int(photo['created']))
 
+        # Get the file...
+        filename = os.path.join(album_path, '%s.jpg' % pid)
+        photo['path'] = filename
+        
         # If file already exists don't download
-        if os.path.isfile(filename): return
+        if os.path.isfile(filename):
+            self.photo_saved(None) # but update counter
+            return
 
-        # skip on 404 error: Issue 13
-        try:
-            picout = open(filename, 'wb')
-            handler = urllib2.Request(photo['src_big'])
-            data = self.retry_function(10, urllib2.urlopen, handler)
-            picout.write(data.read())
-            picout.close()
-            os.utime(filename, (int(photo['created']),) * 2)
-        except Exception, e:
-            self.error(str(e),False)
+        # Add download task to worker queue
+        self.po.apply_async(download, (photo, filename,), callback = self.photo_saved)
 
+    # called when save_photo task is **successfully** completed
+    def photo_saved(self, r):
+        self.index += 1
+        self.update(self.index, self.total)
+
+    # the main show
     def run(self):
         try:
             self.get_albums()
@@ -257,12 +190,44 @@ class FBDownloader(Thread):
             if not os.path.isdir(self.photos_path):
                 os.makedirs(self.photos_path)
 
+            # save each album
             for album in self.albums.values():
                 self.save_album(album)
+
+            # save data to file
+            albumhelpers.save_albums(self.albums, self.photos_path)
         except Exception, e:
+            logging.exception('problem in download thread')
             self.exit_if_terminated()
-            # print 'DL caught exception', e
-            self.error(e)
             self.force_exit() # kill GUI
             sys.exit(1) # kill thread
-        self.update(self.index,self.total)
+        self.update(self.index,self.total,done=True)
+
+def download(photo, filename):
+    max_retries = 10
+    retries = 0
+    
+    picout = open(filename, 'wb')
+    handler = urllib2.Request(photo['src_big'])
+    retry = True
+    
+    while retry:
+        try:
+            data = urllib2.urlopen(handler)
+            retry = False
+        except Exception, e:
+            if retries < max_retries:
+                retries += 1
+                logging.debug('retrying download %s' % filename)
+                # sleep longer and longer between retries
+                time.sleep(retries * 2)
+            else:
+                # skip on 404 error: Issue 13
+                logging.warning('Could not download %s' % filename)
+                picout.close()
+                os.remove(filename)
+                return
+
+    picout.write(data.read())
+    picout.close()
+    os.utime(filename, (int(photo['created']),) * 2)
